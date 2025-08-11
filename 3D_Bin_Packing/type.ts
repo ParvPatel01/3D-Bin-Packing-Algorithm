9847196631ed14e5ac711b77083529067b31a589
ParvPatel01 <patel.parv1@northeastern.edu>
2025-08-11 00:49:21 -0500

    Adding Types For the algorithm

---
+++ 3D_Bin_Packing/type.ts
@@ -0,0 +1,25 @@
+export interface Box {
+    id: string;
+    w: number;
+    h: number;
+    d: number;
+    qty: number;
+}
+
+export interface PlacedBox extends Box {
+    x: number;
+    y: number;
+    z: number;
+    orientation: [number, number, number]; // [bw, bh, bd]
+}
+
+export interface Pallet {
+    w: number;
+    h: number;
+    d: number;
+}
+
+export interface LayerCandidate {
+    height: number;
+    evalScore: number;
+}
